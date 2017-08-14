6aed521c738ae28abbb741bf56236fe295ab694a
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-14 01:51:18 -0500

    Problem #7

--- src/Arrays_Level2.java
+++ src/Arrays_Level2.java
@@ -140,6 +140,25 @@ no14([2, 3, 4]) → true
 		  return false;
 		}
 
+/*Problem #7
+ 
+We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array, at least one of the pair is that value. Return true if the given value is everywhere in the array.
+
+isEverywhere([1, 2, 1, 3], 1) → true
+isEverywhere([1, 2, 1, 3], 2) → false
+isEverywhere([1, 2, 1, 3, 4], 1) → false 
+ */
+	public static boolean isEverywhere(int[] nums, int val) {
+		  
+		  for(int i=1; i< nums.length; i++)
+		  {
+		    if(nums[i] != val && nums[i-1] != val)
+		      return false;
+		    
+		  }
+		  
+		  return true;
+		}
 	
 	
 	
