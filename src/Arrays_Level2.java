4c17f5c3b7b136fa50e2b371deea3a53a1eba189
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-14 02:20:05 -0500

    New Problem / updated

--- src/Arrays_Level2.java
+++ src/Arrays_Level2.java
@@ -167,7 +167,7 @@ either24([1, 2, 2]) → true
 either24([4, 4, 1]) → true
 either24([4, 4, 1, 2, 2]) → false	
  */
-	public boolean either24(int[] nums) {
+	public static boolean either24(int[] nums) {
 		  int twos = 0;
 		  int fours = 0;
 		  
@@ -183,6 +183,45 @@ either24([4, 4, 1, 2, 2]) → false
 		  }
 		  
 		  return (twos > fours || fours > twos);
+		}
+/* Problem #9
+ 
+Given arrays nums1 and nums2 of the same length, for every element in nums1, consider the corresponding element in nums2 (at the same index). Return the count of the number of times that the two elements differ by 2 or less, but are not equal.
+
+matchUp([1, 2, 3], [2, 3, 10]) → 2
+matchUp([1, 2, 3], [2, 3, 5]) → 3
+matchUp([1, 2, 3], [2, 3, 3]) → 2
+ */
+	public static int matchUp(int[] nums1, int[] nums2) {
+		  int differByTwo = 0;
+		  
+		  for(int i=0; i < nums1.length; i++)
+		  {
+		    if(nums1[i] != nums2[i] && Math.abs(nums1[i] - nums2[i]) < 3)
+		      differByTwo++;
+		  }
+		  
+		  return differByTwo;
+		}
+	
+/* Problem #10
+Given an array of ints, return true if the array contains two 7's next to each other, or there are two 7's separated by one element, such as with {7, 1, 7}.
+
+has77([1, 7, 7]) → true
+has77([1, 7, 1, 7]) → true
+has77([1, 7, 1, 1, 7]) → false
+ */
+	public static boolean has77(int[] nums) {
+		  for(int i=1; i< nums.length-1; i++)
+		  {
+		    if(nums[i] == 7 && (nums[i-1]==7 || nums[i+1]==7))
+		      return true;
+		    else if(nums[i-1]==7 && nums[i+1]==7)
+		      return true;
+		      
+		  }
+		  
+		  return false;
 		}
 
 	
