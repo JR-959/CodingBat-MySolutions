73d679e30f5387a4c6a72a85455da1eb85e44e4e
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-15 17:49:56 -0500

    Pr #2 & Pr #3

--- src/Recursion_Level1.java
+++ src/Recursion_Level1.java
@@ -20,7 +20,66 @@ powerN(3, 3) → 27
 		    return base * powerN(base, n-1);
 		}
 
-/*
- 
+/*Problem #2
+Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+
+countX("xxhixx") → 4
+countX("xhixhix") → 3
+countX("hi") → 0
 */
+	public static int countX(String str) {
+		  if(str.length() == 0)
+		    return 0;
+		    
+		  String letter = str.substring(0,1);
+		  String theRest = str.substring(1);  
+		    
+		  if(letter.equals("x"))
+		    return 1+countX(theRest);
+		  else
+		    return 0 + countX(theRest);
+		}
+
+
+/*Problem #3
+ Given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+
+countHi("xxhixx") → 1
+countHi("xhixhix") → 2
+countHi("hi") → 1
+*/
+public int countHi(String str) {
+	  if(str.length() == 0 || str.length() <2)
+	    return 0;
+	    
+	  String letter = str.substring(0,2);
+	  String theRest = str.substring(1);  
+	    
+	  if(letter.equals("hi"))
+	    return 1+countHi(theRest);
+	  else
+	    return 0 + countHi(theRest);
+
 }
+
+
+}// end of Recursion_Level1.java	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
