f0e3aa192b37c1c66f174f3e2ff417d3add2ace8
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-18 11:20:18 -0500

    Problem #6

--- src/Recursion_Level1.java
+++ src/Recursion_Level1.java
@@ -92,7 +92,25 @@ public int sumDigits(int n) {
 	  
 	}
 
-	
+/*Problem #6
+Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+
+changePi("xpix") → "x3.14x"
+changePi("pipi") → "3.143.14"
+changePi("pip") → "3.14p"
+ */
+
+public static String changePi(String str) {
+	  if(str.length() == 0)
+	    return "";
+	  if(str.length() > 1 && str.substring(0,2).equals("pi")) 
+	      return ("3.14" + changePi(str.substring(2)));
+	  else
+	      return str.substring(0,1) + changePi(str.substring(1));
+	  
+	}
+
+
 }// end of Recursion_Level1.java	
 
 
