415ba8db535f5578cbb1ac60ea6e448d49bb8656
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-21 11:27:10 -0500

    PR #17

--- src/Recursion_Level1.java
+++ src/Recursion_Level1.java
@@ -287,10 +287,25 @@ public int count11(String str) {
 	    return count11(str.substring(1));
 	}
 
+
+
+/* Problem #17
+Given a string, return recursively a "cleaned" string where adjacent chars that are the same have been reduced to a single char. So "yyzzza" yields "yza".
+
+stringClean("yyzzza") → "yza"
+stringClean("abbbcdd") → "abcd"
+stringClean("Hello") → "Helo" 
+*/
+public String stringClean(String str) {
+	  if(str.length() == 0)
+	    return "";
+	  else if(str.length() > 1 && str.substring(0,1).equals(str.substring(1,2)))
+	    return stringClean(str.substring(1));
+	  else
+	    return str.substring(0,1) + stringClean(str.substring(1));
+	}
+
 }// end of Recursion_Level1.java	
-
-
-
 
 
 
