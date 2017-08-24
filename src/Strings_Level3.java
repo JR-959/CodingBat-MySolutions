e5c18c9a278560de40e9e702ef7ba1b00f869fb7
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-24 09:51:22 -0500

    PR #5

--- src/Strings_Level3.java
+++ src/Strings_Level3.java
@@ -79,5 +79,30 @@ stringX("xabxxxcdx") → "xabcdx"
 		  }
 		  return newOne;
 		}
+/* Problem #5 
+Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+stringYak("yakpak") → "pak"
+stringYak("pakyak") → "pak"
+stringYak("yak123ya") → "123ya"
+ */
+	public String stringYak(String str) {
+		  String newOne = "";
+		  int i =0;
+		  
+		  while(i < str.length())
+		  {
+		    if(i+2 < str.length() && str.charAt(i) == 'y' && str.charAt(i+2) == 'k')
+		      i+=3;
+		    else
+		    {
+		      newOne+= str.charAt(i);
+		      i++;
+		    }
+		  }
+		  
+		  return newOne;
+		}
+
 	
 }
