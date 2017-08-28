c64c746c82dd1c46ea84b42be1974db631b5f49d
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-27 23:41:32 -0500

    PR #9

--- src/Strings_Level3.java
+++ src/Strings_Level3.java
@@ -167,7 +167,22 @@ public String mixString(String a, String b) {
 	}
 
 /* Problem #9
-* 
+Given a string and an int n, return a string made of the first n characters of the string, followed by the first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, inclusive (i.e. n >= 0 and n <= str.length()).
+
+repeatFront("Chocolate", 4) → "ChocChoChC"
+repeatFront("Chocolate", 3) → "ChoChC"
+repeatFront("Ice Cream", 2) → "IcI"
 */
+
+public String repeatFront(String str, int n) {
+	  String newOne = "";
+	  for(int i = n ; i>=0 ;i--)
+	  {
+	    newOne += str.substring(0,i);
+	  }
+	  
+	  return newOne;
+	}
+
 	
 }// end of Strings_Level3 Class
