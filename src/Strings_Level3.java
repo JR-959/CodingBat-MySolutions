ccbfd3875db4b0b14946980d02ae8089e29802ad
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-28 20:40:22 -0500

    PR #13

--- src/Strings_Level3.java
+++ src/Strings_Level3.java
@@ -261,10 +261,23 @@ public String getSandwich(String str) {
 	    return "";
 	}
 
-/*
-  
-*/
+/*Problem #13
+Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
 
+oneTwo("abc") → "bca"
+oneTwo("tca") → "cat"
+oneTwo("tcagdo") → "catdog"
+*/
+public String oneTwo(String str) {
+	  String newStr ="";
+	  for(int i=0; i< str.length()-2 ;i = i+3)
+	  {
+	      newStr += str.substring(i+1, i+3);
+	      newStr += str.charAt(i);
+	  }
+	  
+	  return newStr;
+	}
 
 
 
