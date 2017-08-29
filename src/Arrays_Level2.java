b46b63c13df977e6a121634f8f7e629ba6f8f9dc
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-29 11:51:31 -0500

    PR #16

--- src/Arrays_Level2.java
+++ src/Arrays_Level2.java
@@ -333,9 +333,28 @@ sum67([1, 1, 6, 7, 2]) → 4
 		  return sum;
 		}
 
-/*
-  	
+/* Problem #16
+Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+
+haveThree([3, 1, 3, 1, 3]) → true
+haveThree([3, 1, 3, 3]) → false
+haveThree([3, 4, 3, 3, 4]) → false  	
 */
+	public boolean haveThree(int[] nums) {
+		  int count = 0;
+
+		  for(int i=0; i< nums.length; i++)
+		  {
+		    if(nums[i] == 3)
+		      count++;
+		    
+		    if(i+1 < nums.length && nums[i] == 3 && nums[i+1] == 3)
+		      return false;
+		  }
+		  
+		  return (count == 3);
+		}
+		
 	
 	
 	
