b85d8911283d16cbc5c9675663e24d6dc601ebe9
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-09-12 17:32:52 -0500

    PR #24

--- src/Recursion_Level1.java
+++ src/Recursion_Level1.java
@@ -470,7 +470,35 @@ strCopies("catcowcat", "cow", 1) → true
 		    
 		}
 
-	
+
+/* Problem #24
+Given a string and a non-empty substring sub, compute recursively the largest substring which starts and ends with sub and return its length.
+
+strDist("catcowcat", "cat") → 9
+strDist("catcowcat", "cow") → 3
+strDist("cccatcowcatxx", "cat") → 9
+*/
+	public int strDist(String str, String sub) {
+		  
+		  if(str.startsWith(sub) && str.endsWith(sub))
+		    return str.length();
+		    
+		  if(str.length() <= sub.length())
+		    return 0;  
+		  else
+		  {
+		    int start = 0;
+		    int end = str.length();
+		    if(str.length()> 1){
+		      if(!str.startsWith(sub))
+		        start+= 1;
+		      if(!str.endsWith(sub))
+		        end-= 1;
+		    }  
+		    return strDist(str.substring(start,end), sub);  
+		  }
+		}
+
 }// end of Recursion_Level1.java	
 
 
