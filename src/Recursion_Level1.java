4b594c2dcfb7d52ae061ea720eafc2a57e176701
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-09-11 19:45:41 -0500

    PR #23

--- src/Recursion_Level1.java
+++ src/Recursion_Level1.java
@@ -425,9 +425,27 @@ nestParen("(((x))") → false
 		}
 	
 /*Problem #22
- 
+Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the sub strings overlapping.
+
+strCount("catcowcat", "cat") → 2
+strCount("catcowcat", "cow") → 1
+strCount("catcowcat", "dog") → 0 
 */
-	
+	public int strCount(String str, String sub) {
+		  if(str.length() < sub.length())
+		    return 0;
+		  
+		  String rest = str.substring(sub.length());
+		  if(str.substring(0,sub.length()).equals(sub)) 
+		    return 1+ strCount(rest,sub);
+		  else{
+		    rest = str.substring(1);
+		    return strCount(rest,sub);
+		  }
+		    
+
+		}
+
 	
 }// end of Recursion_Level1.java	
 
